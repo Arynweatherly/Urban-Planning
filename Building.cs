5abbe119c51683c353b4deadc1eda53a91589595
aryn weatherly <arynweatherly@aryns-MacBook-Pro.local>
2019-11-19 15:08:08 -0600

    first commit

---
+++ Building.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Planner
+{
+    public class Building
+    {
+        //private field _designer of type string(your name)
+        //_dateConstructed of type DateTime. hold exact time building created.
+        // _address type string, _owner type string (store name of person who owns building)
+        private string _designer = "Aryn";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        //Constructor-special method in a class that is called when a new instance of the class is created. The role of a constructor is to make sure the new object is setup and ready for use immediately after it is created.
+
+        public Building(string address, string name)
+        {
+            _address = address;
+            Name = name;
+        }
+        //public properties
+        public string Name { get; set; }
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public string Volume
+        {
+            get
+            {
+                return ($"{Width * Depth * (3 * Stories)}");
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return ($"{_address} Designed by: {_designer} Date Constructed: {_dateConstructed} Owned by: {_owner} Volume: {Volume}");
+            }
+        }
+
+        //Methods
+        //define a Construct() method. method's logic should set the _dateConstructed field's value to DateTime.Now
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+        public void Purchase(string name)
+        {
+            _owner = name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            City awesomeCity = new City("awesomeCity")
+            {
+                Mayor = "Phil",
+                Established = 1830
+            };
+
+            City funnyTown = new City("funnyTown")
+            {
+                Mayor = "Sage",
+                Established = 1630
+            };
+
+            Building Old = new Building("123 smokey way", "Old")
+            {
+                Width = 10,
+                Depth = 10,
+                Stories = 20
+            };
+            Building New = new Building("222 heck yeah", "New")
+            {
+                Width = 1234,
+                Depth = 543,
+                Stories = 123
+            };
+            Building Haunted = new Building("666 spooky lane", "Haunted")
+            {
+                Width = 666,
+                Depth = 800,
+                Stories = 6
+            };
+            Building PhilTower = new Building("100 phil drive", "PhilTower")
+            {
+                Width = 123,
+                Depth = 456,
+                Stories = 50
+            };
+            Building KeatonHouse = new Building("123 Keaton Court", "KeatonHouse")
+            {
+                Width = 200,
+                Depth = 200,
+                Stories = 20
+            };
+            Building DylanPlaza = new Building("100 Pickle Drive", "DylanPlaza")
+            {
+                Width = 100,
+                Depth = 200,
+                Stories = 20
+            };
+
+            Old.Construct();
+            Old.Purchase("Nate");
+            //Console.WriteLine(Old.Description);
+            awesomeCity.AddBuilding(Old);
+
+            New.Construct();
+            New.Purchase("Taylor");
+            //Console.WriteLine(New.Description);
+            awesomeCity.AddBuilding(New);
+
+            Haunted.Construct();
+            Haunted.Purchase("Heidi");
+            //Console.WriteLine(Haunted.Description);
+            awesomeCity.AddBuilding(Haunted);
+
+            PhilTower.Construct();
+            PhilTower.Purchase("Phil");
+            //Console.WriteLine(PhilTower.Description);
+            funnyTown.AddBuilding(PhilTower);
+
+            KeatonHouse.Construct();
+            KeatonHouse.Purchase("keaton");
+            //Console.WriteLine(KeatonHouse.Description);
+            funnyTown.AddBuilding(KeatonHouse);
+
+            DylanPlaza.Construct();
+            DylanPlaza.Purchase("Dylan");
+            //Console.WriteLine(DylanPlaza.Description);
+            funnyTown.AddBuilding(DylanPlaza);
+
+            foreach (Building building in awesomeCity.Buildings)
+            {
+                Console.WriteLine($"{awesomeCity.Name} has the following buildings: {building.Name}");
+            };
+
+            foreach (Building building in funnyTown.Buildings)
+            {
+                Console.WriteLine($"{funnyTown.Name} has the following buildings: {building.Name}");
+            };
+
+        }
+    }
+}
